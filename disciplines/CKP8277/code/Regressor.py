0edbfa2abffbc4b6e57f7d7066a6579ec3d5b93b
Madson Dias <madsonddias@gmail.com>
2018-08-28 15:55:58 -0300

    add regularization and minor fixes

--- disciplines/CKP8277/code/Regressor.py
+++ disciplines/CKP8277/code/Regressor.py
@@ -11,66 +11,87 @@ class Regressor(object):
         pass
 
 
-
-
-import numpy as np
+from numpy.linalg import inv
+from numpy.random import randn
+from numpy import dot, eye, mean, zeros, multiply, power, newaxis, min, max, array
 from matplotlib import pyplot as plt
 
 class LinearRegressor(Regressor):
     """docstring for LinearRegressor"""
-    def __init__(self, params={'method':'OLS', 'eta': 0.01, 'epochs': 100}):
+    def __init__(self, params={'method':'OLS', 'lambda': 0}):
         self.params  = params
         self.model   = dict()
         self.metrics = dict()
 
-    def fit(self, data):
-        self.train_data = data
-
-        # ordinary least squares
-        if self.params['method'] == 'OLS':
-            self.model['w'] = np.linalg.pinv(data.X).dot(data.y)
-            self.metrics['mse'] = np.power(data.y - data.X.dot(self.model['w']),2).mean()
+        # set default parameters
+        #  - lambda (regularization parameter): 0
+        #  - eta (learning rate): 0.01
+        #  - epochs (number of epochs): 100
+        params['lambda'] = 0 if 'lambda' not in self.params.keys() else params['lambda']
+        if self.params['method'] in ['SGD', 'GD']:
+            params['eta']    = 0.01 if 'eta' not in self.params.keys() else params['eta']
+            params['epochs'] = 100 if 'epochs' not in self.params.keys() else params['epochs']
         
-        # gradient descendent
+
+    def fit(self, data):
+        self.train_data = data.copy()
+        self.train_data.add_bias()
+        X_ = self.train_data.X
+        y_ = self.train_data.y
+        # initial estimation of w
+        w = 0.01 * randn(X_.shape[1],1)
+        # ordinary least squares with regularization
+        if self.params['method'] == 'OLS':
+            lI = self.params['lambda'] * eye(X_.shape[1])
+            lI[0,0] = 0
+            w = dot(dot(inv(dot(X_.T,X_) + lI),X_.T), y_)
+
+        
+        # gradient descendent with regularization
         elif self.params['method'] == 'GD':
-            self.model['w']     = 0.01 * np.random.randn(data.X.shape[1],1)
-            self.metrics['mse'] = np.zeros((self.params['epochs'],))
-            y_hat               = data.X.dot(self.model['w'])
+            self.metrics['mse_i'] = zeros((self.params['epochs'],))
+            y_hat               = dot(X_,w)
             for i in range(self.params['epochs']):
-                self.model['w']        = self.model['w'] + self.params['eta'] * np.mean(np.multiply(data.y - y_hat, data.X),0)[np.newaxis].T
-                y_hat                  = data.X.dot(self.model['w'])
+                # bias update
+                w[0]     = w[0] + self.params['eta'] * mean(y_ - y_hat)
+                # weigth update
+                w[1:]    = w[1:] + self.params['eta'] * (mean(multiply(y_ - y_hat, X_[:,1:]),0)[newaxis].T - \
+                                         self.params['lambda']/X_.shape[0]*w[1:])
+                y_hat                  = dot(X_, w)
                 # mean squared error
-                self.metrics['mse'][i] = ((data.y - y_hat) ** 2).mean()
+                self.metrics['mse_i'][i] = mean((y_ - y_hat) ** 2)
 
         # stochastic gradient descendent
         elif self.params['method'] == 'SGD':
-            self.model['w'] = 0.01 * np.random.randn(data.X.shape[1],1)
-            self.metrics['mse'] = np.zeros((self.params['epochs'],))
+            self.metrics['mse_i'] = zeros((self.params['epochs'],))
             for i in range(self.params['epochs']):
-                for j in range(data.X.shape[0]):
-                    y_hat_j         = data.X[j,:].dot(self.model['w'])
-                    self.model['w'] = self.model['w'] + self.params['eta'] * np.multiply(data.y[j,:] - y_hat_j, data.X[j,:])[np.newaxis].T
+                for j in range(X_.shape[0]):
+                    y_hat_j         = dot(X_[j,:],w)
+                    w = w + self.params['eta'] * multiply(y_[j,:] - y_hat_j, X_[j,:])[newaxis].T 
                 # mean squared error
-                y_hat                  = data.X.dot(self.model['w'])
-                self.metrics['mse'][i] = ((data.y - y_hat) ** 2).mean()
+                y_hat                  = dot(X_,w)
+                self.metrics['mse_i'][i] = mean((y_ - y_hat) ** 2)
                 data.shuffle()
 
+        self.model['w']     = w
+        self.metrics['mse'] = mean((y_ - dot(X_,self.model['w'])) ** 2)      #np.power(y_ - X_.dot(self.model['w']),2).mean()
+
     def predict(self, X):
-        return X.dot(self.model['w'])
+        return dot(X,self.model['w'])
 
     def plot(self, info='curve'):
         if info == 'curve':
             
-            X_ = np.array([np.min(self.train_data.X,0), np.max(self.train_data.X,0)])
-            plt.title('Linear approximation')
+            X_ = array([min(self.train_data.X,0), max(self.train_data.X,0)])
+            plt.title('Linear approximation (%s)\n(w=[%.3f, %.3f], MSE=%.3f)' % (self.params['method'],self.model['w'][0], self.model['w'][1], self.metrics['mse']))
             plt.xlabel('x')
             plt.ylabel('y')
             plt.scatter(self.train_data.X[:,1], self.train_data.y)
             plt.plot(X_[:,1], self.predict(X_), 'k-')
             plt.show()
         elif info == 'mse':
-            plt.plot(range(self.params['epochs']), self.metrics['mse'])
-            plt.title('Mean Squared Error (w = [%.3f, %.3f])' % (self.model['w'][0], self.model['w'][1]))
+            plt.plot(range(self.params['epochs']), self.metrics['mse_i'])
+            plt.title('Mean Squared Error (%s)\n(w=[%.3f, %.3f], MSE=%.3f)' % (self.params['method'],self.model['w'][0], self.model['w'][1], self.metrics['mse']))
             plt.xlabel('Epochs')
             plt.ylabel('MSE')
             plt.show()
