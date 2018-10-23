7540d5edc941c691b492f60c63f52495b8e3da8a
Madson Dias <madsonddias@gmail.com>
2018-10-23 10:02:15 -0300

    fix bugs.

--- MF_MLM.m
+++ MF_MLM.m
@@ -12,8 +12,8 @@ classdef MF_MLM < MLM
         function obj = fit(obj, X, y)
             obj = obj.get_training_set(X,y);
             
-            D_in  = pdist2(X, X);
-            D_out = pdist2(y, y);
+            D_in  = phi(X, X);
+            D_out = phi(y, y);
             
             % step 1 RP_in pruning
             [B, gamma_ind, ~, ~] = MFOCUSS(D_in, D_out,...
@@ -23,33 +23,6 @@ classdef MF_MLM < MLM
             obj.parameters.rp_index_in  = gamma_ind;
             obj.parameters.rp_index_out = 1:size(X,1);
         end
-%         function cost = in_cost(obj, y, d_out_hat)
-%             d_out = pdist2(y, obj.training_set.y(obj.parameters.rp_index,:));
-%             cost = d_out.^2 - d_out_hat.^2;
-%         end
-%         function y_hat = predict(obj,X)
-%             N         = size(X,1);
-%             y_hat     = zeros(N,1);
-%             X_rp      = obj.training_set.X(obj.parameters.rp_index,:);
-%             D_in      = pdist2(X, X_rp);
-%             D_out_hat = D_in * obj.parameters.B;
-%             y_mean    = mean(obj.training_set.y(obj.parameters.rp_index,:));
-%             
-%             for i=1:N
-%                 d_out_hat  = D_out_hat(i,:);
-%                 J          = @(y) obj.in_cost(y, d_out_hat);
-%                 options    = optimoptions('lsqnonlin','Display','off');
-%                 y_hat(i,1) = lsqnonlin(J,y_mean,[],[],options);
-%             end
-%         end
-%         function plot(obj)
-%             obj.plot@Regressor(100);
-%             hold on;
-%             plot(obj.training_set.X(obj.parameters.rp_index_in,:), ...
-%                  obj.training_set.y(obj.parameters.rp_index_in,:),...
-%                  'ko','MarkerSize',10);
-%             hold off;
-%         end
     end
 end
 
